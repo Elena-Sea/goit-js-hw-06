e0f02ed353343d107857be64666589fe689c7df3
Elena-Sea <smetyukelena@gmail.com>
2022-06-05 17:19:01 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,2 +1,26 @@
 'use strict';
 
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+console.log(decrementBtn);
+const incrementBtn = document.querySelector('[data-action="increment"]');
+console.log(incrementBtn);
+
+const value = document.querySelector('#value');
+console.log(value);
+
+let counterValue = 0;
+
+const incrBtnClickHandler = () => {
+    counterValue += 1;
+    value.textContent = counterValue;
+};
+
+const decrBtnClickHandler = () => {
+    counterValue -= 1;
+    value.textContent = counterValue;
+
+};
+
+incrementBtn.addEventListener("click", incrBtnClickHandler);
+decrementBtn.addEventListener("click", decrBtnClickHandler);
+
