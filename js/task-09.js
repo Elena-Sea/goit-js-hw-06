b1610cf5b8015f7d1b4fe296ee00700da5652e16
Elena-Sea <smetyukelena@gmail.com>
2022-06-06 22:36:06 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+'use strict';
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const body = document.querySelector('body');
+const button = document.querySelector('.change-color');
+const colorName = document.querySelector('.color');
+
+const colorChangeHandler = () => {
+  colorName.textContent = getRandomHexColor();
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+button.addEventListener('click', colorChangeHandler);
