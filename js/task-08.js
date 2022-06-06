3a9f13358fd1ae4b6352b4d5ce2b1d4435453c6e
Elena-Sea <smetyukelena@gmail.com>
2022-06-06 22:09:55 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+'use strict';
 
+const form = document.querySelector('.login-form');
+console.log(form);
+
+let user = {
+    email: '',
+    password: '',
+};
+
+const formHandler = (event) => {
+    event.preventDefault();
+    const { elements: { email, password } } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+         alert("Please fill in all the fields!");
+    } else {
+        user.email = email.value;
+        user.password = password.value;
+        console.log(user);
+    }
+    event.currentTarget.reset();
+}
+
+form.addEventListener("submit", formHandler);
