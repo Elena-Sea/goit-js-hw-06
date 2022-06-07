756ccd3c43489dae5ae9de1f0ac1687e8e4a6e26
Elena-Sea <smetyukelena@gmail.com>
2022-06-07 11:49:27 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -12,13 +12,14 @@ const formHandler = (event) => {
     const { elements: { email, password } } = event.currentTarget;
 
     if (email.value === "" || password.value === "") {
-         alert("Please fill in all the fields!");
+        alert("Please fill in all the fields!");
     } else {
+        
         user.email = email.value;
         user.password = password.value;
         console.log(user);
     }
     event.currentTarget.reset();
-}
+};
 
 form.addEventListener("submit", formHandler);
